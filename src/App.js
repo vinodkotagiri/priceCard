186ab9cc3ac070dd3cac7692bc547b5b7f9ecdbd
vinodkotagiri <vinodkotagiri@icloud.com>
2022-07-03 15:22:39 +0530

    All files Added:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PriceCart from './components/PriceCart';
 
 function App() {
+  let data=[
+    {
+      plan:"FREE",
+      price:"0",
+      user:"Single User",
+      isUser:true,
+      storage:"5GB",
+      isStorage:true,
+      publicProjects:"Unlimited Public Projects",
+      isPublicProject:true,
+      communityAccess:"Community Access",
+      isCommunityAccess:true,
+      privateProjects:"Unimited Private Projects",
+      isPrivateProject:false,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:false,
+      subDomain:"Free Subdomain",
+      isSubDomain:false,
+      reports:"Monthly Status Reports",
+      isReports:false,
+    },
+    {
+      plan:"PLUS",
+      price:"9",
+      user:"5 Users",
+      isUser:true,
+      storage:"50GB",
+      isStorage:true,
+      publicProjects:"Unlimited Public Projects",
+      isPublicProject:true,
+      communityAccess:"Community Access",
+      isCommunityAccess:true,
+      privateProjects:"Unimited Private Projects",
+      isPrivateProject:true,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:true,
+      subDomain:"Free Subdomain",
+      isSubDomain:true,
+      reports:"Monthly Status Reports",
+      isReports:false,
+    },
+    {
+      plan:"PRO",
+      price:"49",
+      user:"Unlimited Users",
+      isUser:true,
+      storage:"50GB",
+      isStorage:true,
+      publicProjects:"Unlimited Public Projects",
+      isPublicProject:true,
+      communityAccess:"Community Access",
+      isCommunityAccess:true,
+      privateProjects:"Unimited Private Projects",
+      isPrivateProject:true,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:true,
+      subDomain:"Unlimited Free Subdomain",
+      isSubDomain:true,
+      reports:"Monthly Status Reports",
+      isReports:true,
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     {/* This snippet uses Font Awesome 5 Free as a dependency. You can download it at fontawesome.io!  */}
+
+<section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+      {data.map((plan)=>{
+        return <PriceCart data={plan}/>
+      })}
     </div>
+  </div>
+</section>
+
+    </>
   );
 }
 
